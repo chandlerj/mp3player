c6016e23b2a3206ae1f075ae335b44aa1bfa3e09
Chandler Justice <a02313187@aggies.usu.edu>
2023-06-14 09:46:29 -0600

    audio playback is a go

--- mp3player/src/main.rs
+++ mp3player/src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
+use std::fs::File;
+use std::io::BufReader;
+use rodio::{Decoder, OutputStream, source::Source};
+
 fn main() {
     let args: Vec<String> = env::args().collect(); //program arguments
     dbg!(&args);
 
-    process_arguments(args);
-
+    // process_arguments(args);
+    play_audio(String::from("/home/chandler/Music/Bitches.mp3"));
 
 }
 
@@ -18,4 +22,17 @@ fn process_arguments(args: Vec<String>){
         let file = &args[1];
         println!("playing file {file}")
     }
+  
+}
+
+fn play_audio(audio_path: String){
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+
+    let file = BufReader::new(File::open(audio_path).unwrap());
+
+    let source = Decoder::new(file).unwrap();
+
+    stream_handle.play_raw(source.convert_samples());
+
+    std::thread::sleep(std::time::Duration::from_secs(5));
 }
